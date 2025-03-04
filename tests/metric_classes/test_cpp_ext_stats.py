86f9934ed4bb7b7a8162467a12e87b44b12f8202
Дмитрий Неганов <neganov.dima@gmail.com>
2025-03-05 00:20:58 +0300

    test(CppExtStats): number_of_files

--- tests/metric_classes/test_cpp_ext_stats.py
+++ tests/metric_classes/test_cpp_ext_stats.py
@@ -1,12 +1,14 @@
 import pytest
 
 from src.cpp_ext_stats import CppExtStats
+from src.metric_classes.number_of_files import NumberOfFiles
 from src.metric_classes.method_hiding_factor import MethodHidingFactor
 from src.metric_classes.attribute_hiding_factor import AttributeHidingFactor
 
 
 class TestCppExtStats:
     __repo_paths = ["tests/data/empty",
+                    "tests/data/gitignore",
                     "tests/data/inheritance/multiple",
                     "tests/data/inheritance/private",
                     "tests/data/inheritance/protected",
@@ -17,7 +19,18 @@ class TestCppExtStats:
 
     @pytest.mark.parametrize(
         "repo_path, expected",
-        list(zip(__repo_paths, [0, 0, 1, 1, 0.5, 10 / 21, 0, 10 / 22])))
+        list(zip(__repo_paths, [0, 10, 1, 2, 2, 2, 8, 1, 22]))
+    )
+    def test_number_of_files(self, repo_path, expected):
+        stats = CppExtStats(repo_path)
+        result = stats.metric(NumberOfFiles.NAME)
+
+        assert result == expected
+
+    @pytest.mark.parametrize(
+        "repo_path, expected",
+        list(zip(__repo_paths, [0, 0, 0, 1, 1, 0.5, 10 / 21, 0, 10 / 22]))
+    )
     def test_method_hiding_factor(self, repo_path, expected):
         stats = CppExtStats(repo_path)
         result = stats.metric(MethodHidingFactor.NAME)
@@ -26,7 +39,8 @@ class TestCppExtStats:
 
     @pytest.mark.parametrize(
         "repo_path, expected",
-        list(zip(__repo_paths, [0, 0, 1, 1, 0.5, 10 / 21, 0, 10 / 21])))
+        list(zip(__repo_paths, [0, 0, 0, 1, 1, 0.5, 10 / 21, 0, 10 / 21]))
+    )
     def test_attribute_hiding_factor(self, repo_path, expected):
         stats = CppExtStats(repo_path)
         result = stats.metric(AttributeHidingFactor.NAME)
