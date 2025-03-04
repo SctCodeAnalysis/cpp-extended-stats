79f3d5466629d8531ee10c5157e6f640c7d3bce5
Дмитрий Неганов <neganov.dima@gmail.com>
2025-03-05 00:26:57 +0300

    test(CppExtStats): number_of_classes

--- tests/metric_classes/test_cpp_ext_stats.py
+++ tests/metric_classes/test_cpp_ext_stats.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.cpp_ext_stats import CppExtStats
 from src.metric_classes.number_of_files import NumberOfFiles
+from src.metric_classes.number_of_classes import NumberOfClasses
 from src.metric_classes.method_hiding_factor import MethodHidingFactor
 from src.metric_classes.attribute_hiding_factor import AttributeHidingFactor
 
@@ -24,6 +25,16 @@ class TestCppExtStats:
     def test_number_of_files(self, repo_path, expected):
         stats = CppExtStats(repo_path)
         result = stats.metric(NumberOfFiles.NAME)
+
+        assert result == expected
+
+    @pytest.mark.parametrize(
+        "repo_path, expected",
+        list(zip(__repo_paths, [0, 0, 4, 2, 2, 2, 11, 4, 16]))
+    )
+    def test_number_of_classes(self, repo_path, expected):
+        stats = CppExtStats(repo_path)
+        result = stats.metric(NumberOfClasses.NAME)
 
         assert result == expected
 
